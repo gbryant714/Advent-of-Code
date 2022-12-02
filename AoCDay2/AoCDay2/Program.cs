cc745e97da339cc07737949a9079d0461a5b82fe
Grace Bryant <95504446+gbryant714@users.noreply.github.com>
2022-12-02 15:33:57 -0500

    Add files via upload

---
+++ AoCDay2/AoCDay2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Advent of Code 2022
+ * Rock Paper Scissors, Day 2
+ */
+
+Dictionary<string, int> results
+{
+    { "A X",  }
+};
+
+var data = File.ReadAllLines("day2.txt");
+
+foreach (var line in data)
+{
+    var results = line.Split(' ');
+
+    var opponent = results[0];
+    var self = results[1];
+}
+
+var stop = 0;
