bc8bb7e0ab66d786924854fe5486eaaf38e15066
Grace Bryant <95504446+gbryant714@users.noreply.github.com>
2022-12-02 15:10:11 -0500

    Add files via upload

---
+++ AoC/AoC/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Advent of Code 2022
+ * Calorie calculator
+ */
+
+var read = File.ReadAllText("day1.txt");
+
+var lines = read.Split("\r\n\r\n");
+var calories = new List<int>();
+
+foreach (var line in lines)
+{
+    var cals = line.Split("\r\n").ToList();
+    var tot = 0;
+
+    foreach (var cal in cals)
+    {
+        tot += int.Parse(cal);
+    }
+
+    calories.Add(tot);
+
+}
+
+var most = calories.Max();
+
+Console.WriteLine($"The elf carrying the most calories has {most} calories in their inventory.");
